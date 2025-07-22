a13a0f9eed4956a008f68e9833bfb17cba2aef53
Gideonnut <ngideon538@yahoo.com>
2025-07-22 15:27:33 +0100

    update ClientDemo.tsx

--- src/components/ClientDemo.tsx
+++ src/components/ClientDemo.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-// note: dynamic import is required for components that use the Frame SDK
-const Demo = dynamic(() => import("~/components/Demo").then(mod => mod.Demo), {
-  ssr: false,
-});
-
 export default function ClientDemo() {
-  return <Demo />;
-} 
+  return null;
+}
