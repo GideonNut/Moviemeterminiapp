9c6383ddd830cc1047c5a0299c3cd1c5c05d2f04
josephnk23 <jyawnketia4@gmail.com>
2025-09-21 16:33:13 +0000

    add ThumbsDownIcon and ThumbsUpIcon exports to icons index

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -3,3 +3,5 @@ export { TrophyIcon } from './TrophyIcon';
 export { HomeIcon } from './HomeIcon';
 export { TvIcon } from './TvIcon';
 export { GiftIcon } from './GiftIcon';
+export {ThumbsDownIcon} from './ThumbsDownIcon';
+export {ThumbsUpIcon} from './ThumbsUpIcon';
