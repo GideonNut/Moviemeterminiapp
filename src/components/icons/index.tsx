12042a5addc758179cd1b91e8c3826084aace0ac
josephnk23 <jyawnketia4@gmail.com>
2025-09-21 13:08:36 +0000

    add index file to export icon components

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export { BellIcon } from './BellIcon';
+export { TrophyIcon } from './TrophyIcon';
+export { HomeIcon } from './HomeIcon';
+export { TvIcon } from './TvIcon';
+export { GiftIcon } from './GiftIcon';
