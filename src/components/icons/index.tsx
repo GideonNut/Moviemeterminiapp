909748ccbb2169bebe567e8df833602c5ea337ef
josephnk23 <jyawnketia4@gmail.com>
2025-09-29 16:43:20 +0000

    add RewardsIcon export to icons index

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -5,3 +5,4 @@ export { TvIcon } from './TvIcon';
 export { GiftIcon } from './GiftIcon';
 export {ThumbsDownIcon} from './ThumbsDownIcon';
 export {ThumbsUpIcon} from './ThumbsUpIcon';
+export { RewardsIcon } from './RewardsIcon';
