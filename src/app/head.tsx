29f214ec0d60dd6f2094a4b364bf86160c0c80cc
Gideonnut <ngideon538@yahoo.com>
2026-04-14 14:23:05 +0100

    feat: create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="talentapp:project_verification"
+        content="36e39149fe5b5e09fed2e247875ce5166dbbac1ace8f51586dbfce0e6a3836a1c5c381ba9a2b8555c61193f4e82972ff4e129b60e69ec36e20cf180252a049ec"
+      />
+    </>
+  );
+}
